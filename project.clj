d4ddbb039033fb9700fad1b3dbce32b7019562d9
Baishampayan Ghose <b.ghose@gmail.com>
2015-08-14 16:51:34 +0530

    Release 0.11.0
    Also, bump clojure.data.json dep.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject helpshift/sensu-client "0.10.0"
+(defproject helpshift/sensu-client "0.11.0"
   :description "Library to send alerts to sensu-client."
   :url "https://www.helpshift.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.2.5"]]
+                 [org.clojure/data.json "0.2.6"]]
   :global-vars {*warn-on-reflection* true}
   :plugins [[codox "0.8.10"]]
   :codox {:defaults {:doc/format :markdown}
