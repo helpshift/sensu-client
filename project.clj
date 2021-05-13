a63c3fc151bec2da3158b5562b6a1aabb33b54fd
Cartmanishere <apbetahouse45@gmail.com>
2021-05-13 14:23:26 +0530

    Update clojure and add deps clj-http, cheshire

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :url "https://www.helpshift.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.2.6"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [cheshire "5.10.0"]
+                 [clj-http "3.12.0"]]
   :global-vars {*warn-on-reflection* true}
   :plugins [[codox "0.8.10"]]
   :codox {:defaults {:doc/format :markdown}
