80b5ce5cf6cc03b6b6e75eaa6210673e6fe96d12
Vedang Manerikar <ved.manerikar@gmail.com>
2021-12-15 22:58:30 +0530

    Update all deps to the latest versions
    - Move Clojure to :dev :dependencies
    - Upgrade cheshire from 5.10.0 to 5.10.1
    - Upgrade clj-http from 3.12.0 to 3.12.3

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://www.helpshift.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [cheshire "5.10.0"]
-                 [clj-http "3.12.0"]]
+  :dependencies [[cheshire "5.10.1"]
+                 [clj-http "3.12.3"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.3"]]}}
   :global-vars {*warn-on-reflection* true}
   :plugins [[codox "0.8.10"]]
   :codox {:defaults {:doc/format :markdown}
